8a21b9e854f159a1d8fa2bdfd595c06e3d6b229f
Adrakali <r.kallyny@gmail.com>
2022-05-06 10:44:08 +0200

    Second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const btns = document.querySelector(".btns")
+
+const tasks = [
+    {task: "Wash Car",  price:  10},
+    {task: "Mow Lawn", price:  20},
+    {task: "Pull Weeds", price:  30}
+] 
+
+btns.addEventListener('click', (e) => {
+    let target = e.target
+    
+})
