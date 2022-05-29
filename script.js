d9eab150c6c289a97f640dcae3ebc782ef0d4c10
Adrakali <r.kallyny@gmail.com>
2022-05-29 17:36:57 +0200

    last commit

--- script.js
+++ script.js
@@ -1,64 +1,48 @@
-const btn = document.querySelectorAll('.item-btn');
+const btns = document.querySelectorAll('[data-item]');
 const tableElement = document.querySelector('.task-table');
 const totalElement = document.querySelector('.total-amount');
-const deleteItem = document.querySelector('.delete');
 let sum = parseInt(totalElement.textContent.replace('$', ''));
-
-//ITEMS ARRAY =====
-const products = [
-	{ product: 'Wash Car', price: 10 },
-	{ product: 'Mow Lawn', price: 20 },
-	{ product: 'Pull Weeds', price: 30 },
-];
+const rCol = document.querySelectorAll('.right');
 
 // ADD ITEM =====
-btn.forEach(function (item) {
-	item.addEventListener('click', (e) => {
-		let target = e.target;
 
-		for (let i = 0; i < products.length; i++) {
-			let item = products[i].product;
-			let price = products[i].price;
-			if (target.value === item) {
-				renderItem(item, price);
-			}
-			target.disabled = true;
-		}
-		deleteRow(item);
+btns.forEach((button) => {
+	let item = button.getAttribute('data-item');
+	let price = parseInt(button.getAttribute('data-price'));
+	button.addEventListener('click', () => {
+		renderItem(item, price);
+		updateTotal(price);
+		button.disabled = true;
 	});
 });
 
+// DELETE ITEM ROW =====
+
+tableElement.addEventListener('click', (e) => {
+	const itemVal = e.target.parentElement.getAttribute('data-item');
+	const targetBtn = document.querySelector(`[data-item="${itemVal}"]`);
+	const price = e.target.parentElement.getAttribute('data-price');
+	if (e.target.classList.contains('delete')) {
+		e.target.parentElement.parentElement.remove();
+		targetBtn.disabled = false;
+		totalElement.textContent = `$ ${(sum -= price)}`;
+	}
+});
+
 //RENDER ITEM =====
+
 function renderItem(currentItem, currentPrice) {
 	let template = `
             <tr class="item-row">
-            <th class="left" value="${currentItem}">${currentItem}</th>
-            <th class="right">$${currentPrice} <i class="fa-solid fa-trash-can delete"></i> </th>
+            <th class="left">${currentItem}</th>
+            <th class="right" data-item="${currentItem}" data-price="${currentPrice}">$${currentPrice} <i class="fa-solid fa-trash-can delete"></i> </th>
             </tr>
             `;
 	tableElement.innerHTML += template;
 }
 
 //UPDATE TOTAL =====
-function updateTotal() {
-	// let cartItem = tableElement[0];
-	// let cartRow = cartItem.getElementByClassName('cart-row');
-	// for (let i = 0; i < cartRow.length; i++) {
-	// 	let cartRows = cartRow[i];
-	// 	console.log(cartItem);
-	// }
-}
-
-// DELETE ITEM ROW =====
-function deleteRow(btnItem) {
-	tableElement.addEventListener('click', (e) => {
-		if (e.target.classList.contains('delete')) {
-			e.target.parentElement.parentElement.parentElement.remove();
-			updateTotal();
-		}
-		// btnItem.value = itemAttribute;
-		// let itemAttribute = btnItem.getAttribute('value');
-		//let resetItemBtn = `${btnItem}.getAttribute('${itemAttribute}')`;
-		btnItem.disabled = false;
-	});
+function updateTotal(itemValue) {
+	totalElement.textContent = `$ ${(sum += itemValue)}`;
+	console.log(typeof sum);
 }
